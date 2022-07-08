da0962eb6d3b205560018cacb20b517771c548f4
unknown <kagonanami@gmail.com>
2022-07-08 23:36:55 +0700

    a

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return 
+  <BrowserRouter>
+  <Routes>
+    <Route path ="/" element={<div>Home page</div>}/>
+    <Route path ="/about" element={<div>
+      <h2>about</h2>
+    </div>}/>
+  </Routes>
+  </BrowserRouter>
+}
+
+export default App;
